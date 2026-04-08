f49088722a6e00fdc7dc90d08b57e9c03f835ab1
Arjun Kapoor <01arjunkapoor@gmail.com>
2026-04-09 04:01:56 +0530

    CLI updated and made better, added checks for file not found, checked basic structure, added a verbose command

--- cli/commands.py
+++ cli/commands.py
@@ -7,17 +7,33 @@ app = typer.Typer()
 
 
 @app.command()
-def generate(input_file: str, output: str = "output.tf"):
+def generate(input_file: str, output: str = "output.tf", verbose: bool = False):
     """
     Generate Terraform from input JSON
     """
-    with open(input_file) as f:
-        config = json.load(f)
+    
+    # Basic file not found check
+    try:
+        with open(input_file) as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        print("[red] File not found[/red]")
+        raise typer.Exit()
+    
+    
+    # Basic structure check of source file
+    if "s3" not in config and "cloudfront" not in config:
+        print("[red]Invalid input: No supported resources found[/red]")
+        raise typer.Exit()
 
     tf_code = generate_all(config)
-
-    with open(output, "w") as f:
+    
+    with open("outputs/"+output, "w") as f:
         f.write(tf_code)
+
+    # Show command integrated with generate
+    if (verbose):
+        print(tf_code)
 
     print("[green]Terraform generated: output.tf[/green]")
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from cli.commands import app
+from rich import print
 
 if __name__ == "__main__":
+    print("[bold blue]----------- TerraMake 1.O -----------[/bold blue]\n")
     app()
